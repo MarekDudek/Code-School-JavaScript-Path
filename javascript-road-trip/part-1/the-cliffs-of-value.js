0a60a43f62b041e7397f6f4325b6c30458e7867b
Marek Dudek <marek.dudek@gmail.com>
2015-01-30 21:44:44 +0100

    The Cliffs of Value done

--- javascript-road-trip/part-1/the-cliffs-of-value.js
+++ javascript-road-trip/part-1/the-cliffs-of-value.js
@@ -1,5 +1,5 @@
 
-print("The cliffs of value");
+print("The cliffs of value")
 
 // operators
 
@@ -25,3 +25,27 @@ var lessThan = 9 < 5
 
 var greaterOrEqual = 8 >= -2
 var lesserOrEqual = 10 <= 10
+
+
+// strings
+
+var string1 = "String can be enclosed in quotes"
+var string2 = 'it can also be enclosed with apostrophes'
+
+var concatenated = string1 + ' ' + string2
+
+var string3 = 'The meaning of life is ' + 42
+
+var string4 = 'String contains \t tabulator'
+
+var string5 = "String contains \" escaped quote"
+var string6 = 'String contains \' escaped apostrophe'
+
+var string7 = "Apostrophe does not ' need to be escaped here"
+var string8 = 'Quote does not " need to be escaped here'
+
+var string9 = 'String contains single \\ backslash'
+
+var string10 = 'String contains \n new line'
+
+var length = 'antidisestablishmentarianism'.length
