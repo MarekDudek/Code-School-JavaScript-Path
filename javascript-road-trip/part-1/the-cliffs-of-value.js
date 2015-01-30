2fc400e29ca89a165bdbd8aac9033fd00137103c
Marek Dudek <marek.dudek@gmail.com>
2015-01-30 21:25:22 +0100

    Numbers and operators

--- javascript-road-trip/part-1/the-cliffs-of-value.js
+++ javascript-road-trip/part-1/the-cliffs-of-value.js
@@ -1,6 +1,8 @@
 
 print("The cliffs of value");
 
+// operators
+
 var addition = 6 + 4;
 var subtraction =  9 - 5
 
@@ -8,3 +10,18 @@ var multiplication = 3 * 4
 var division = 12 / 4
 
 var modulus = 43 % 10
+
+// order of operators
+
+var expression = (3 * 4) + 3 - 12 / 2
+
+// comparators
+
+var equal = 3 == 4
+var notEqual = 12 != 4
+
+var greaterThan = 6 > 4
+var lessThan = 9 < 5
+
+var greaterOrEqual = 8 >= -2
+var lesserOrEqual = 10 <= 10
