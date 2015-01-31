dbaff5b6a78b1d6ff3a081a2dd06dc4d1bbdf7b9
Marek Dudek <marek.dudek@gmail.com>
2015-01-31 18:21:55 +0100

    While loops

---
+++ javascript-road-trip/part-2/the-labyrinth-of-loops.js
@@ -0,0 +1,28 @@
+
+// while loop
+
+var i   = 0;
+var sum = 0;
+
+while (i < 10) {
+	i++;
+	sum += i;
+}
+
+var num = 10;
+while (num > 0) {
+	console.log(num);
+	num--;
+}
+
+var numSheep = 4;
+var monthNumber = 1;
+var monthsToPrint = 12;
+
+while (monthNumber <= monthsToPrint) {
+	numSheep *= 4;
+	console.log('There will be ' + numSheep + ' sheep after ' + monthNumber + ' month(s)!');
+	monthNumber++;
+}
+
+// for loop
