5a77dad37ec11905d8db56b800055569028df980
Marek Dudek <marek.dudek@gmail.com>
2015-01-31 23:22:50 +0100

    Function Expressions and Anonymouns Functions done

--- javascript-road-trip/part-3/forest-of-function-expressions.js
+++ javascript-road-trip/part-3/forest-of-function-expressions.js
@@ -3,3 +3,29 @@
  */
 
 print('Forest of Function Expressions');
+
+// loads only when program reaches this line of code !!!
+var diff = function diffOfSquares(a, b) {
+	return a*a - b*b;
+};
+
+// used by declared variable number, name used to declare function is not defined !!!
+diff(9, 5);
+
+// anonymous function
+var diff2 = function (a, b) {
+	return a*a - b*b;
+};
+
+diff2(9, 5);
+
+// prints the contents of function
+print(diff2);
+
+// calling function passed
+var compute = function(computation, a, b) {
+	return computation(a, b);
+}
+
+var result = compute(diff2, 9, 5);
+print(result);
