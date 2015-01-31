5a7449d89392c3a7af8b222e7121dd325f7bcbb1
Marek Dudek <marek.dudek@gmail.com>
2015-01-31 22:38:44 +0100

    Roudtrip Part 2 done

--- javascript-road-trip/part-2/the-array-archipelago.js
+++ javascript-road-trip/part-2/the-array-archipelago.js
@@ -7,17 +7,20 @@ var passanger1 = passangers[0];
 passangers[3] = 'Olivier Local';
 
 passangers[4] = 'Ashley Smith';
-print(passangers);
 
 var lastPassanger = passangers.pop();
-print(lastPassanger);
-print(passangers);
 
 passangers.push('Adam Rensel');
-print(passangers);
 
 var comboArray = ['One', 'fish', 2, 'fish'];
 
 var arrayOfArrays = [passangers, comboArray];
 var element = arrayOfArrays[1][2];
-print(element);
+
+passangers[3] = undefined;
+
+for (var i=0; i < passangers.length; i++) {
+}
+
+var emptyArray = [];
+
