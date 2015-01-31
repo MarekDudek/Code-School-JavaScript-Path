9ec3bee05e9f4b579cbbc0a4beb3c241e10b399e
Marek Dudek <marek.dudek@gmail.com>
2015-01-31 19:32:14 +0100

    Conditional Canyon cleared

--- javascript-road-trip/part-2/the-labyrinth-of-loops.js
+++ javascript-road-trip/part-2/the-labyrinth-of-loops.js
@@ -61,3 +61,48 @@ for(var monthNumber = 1; monthNumber <= monthsToPrint; monthNumber++) {
 	console.log("There will be " + numSheep + " sheep after " + monthNumber + " month(s)!");
 }
 
+
+// Loop Echo challange #1
+
+console.log('###########################################################################3#');
+
+var numSheep = 4;
+var monthsToPrint = 12;
+
+for(var monthNumber = 1; monthNumber <= monthsToPrint; monthNumber++) {
+
+	if (monthNumber % 4 == 0) {
+		var removed = numSheep * 3/4;
+		console.log('Removing ' + removed + ' sheep from the population.');
+		numSheep -= removed;
+	} else if (numSheep > 10000) {
+		var removed = numSheep / 2;
+		console.log('Removing ' + removed + ' sheep from the population.');
+		numSheep -= removed;
+	}
+
+
+	numSheep *= 4;
+	console.log("There will be " + numSheep + " sheep after " + monthNumber + " month(s)!");
+}
+
+// Loop Echo challange #2
+
+console.log('###########################################################################3#');
+
+var totalGen = 19;
+var totalMW = 0;
+
+for (var currentGen = 1; currentGen <= totalGen; currentGen++) {
+	if (currentGen % 2 == 0) {
+		if (currentGen <= 4) {
+			totalMW += 62;
+			console.log('Generator #' + currentGen + ' is on, adding 62 MW, for a total of ' + totalMW + ' MW!');
+		} else {
+			totalMW += 124;
+			console.log('Generator #' + currentGen + ' is on, adding 124 MW, for a total of ' + totalMW + ' MW!');
+		}
+	} else {
+		console.log('Generator #' + currentGen + ' is off.');
+	}
+}
