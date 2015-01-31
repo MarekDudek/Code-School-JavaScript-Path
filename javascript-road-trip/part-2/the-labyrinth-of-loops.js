b65a9ea9798cfe3e11f9fac2765aeb84508f116a
Marek Dudek <marek.dudek@gmail.com>
2015-01-31 18:38:22 +0100

    Level 1 cleared

--- javascript-road-trip/part-2/the-labyrinth-of-loops.js
+++ javascript-road-trip/part-2/the-labyrinth-of-loops.js
@@ -26,3 +26,22 @@ while (monthNumber <= monthsToPrint) {
 }
 
 // for loop
+
+for (var i=1; i <= 10; i++) {
+	console.log(i);
+}
+
+var currentGen = 1;
+var totalGen = 19;
+var totalMW = 0;
+
+while (currentGen <= 4) {
+	totalMW += 62;
+	console.log('Generator #' + currentGen + ' is on, adding 62 MW, for a total of ' + totalMW + ' MW!');
+	currentGen++;
+}
+
+for ( ; currentGen <= 19; currentGen++) {
+	totalMW += 124;
+	console.log('Generator #' + currentGen + ' is on, adding 124 MW, for a total of ' + totalMW + ' MW!');
+}
