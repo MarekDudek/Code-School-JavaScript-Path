0cd30b598f6925b8208534cbec114c4359bcbbb2
Marek Dudek <marek.dudek@gmail.com>
2015-02-01 16:18:00 +0100

    Challenge III for Hoisting Hills copied

--- javascript-road-trip/part-3/hoisting-hills.js
+++ javascript-road-trip/part-3/hoisting-hills.js
@@ -93,7 +93,34 @@ function theBridgeOfHoistingDoom () {
 
 
 
+//print$(theBridgeOfHoistingDoom());
+
+
+
+function theBridgeOfHoistingDoom( ){
+  function fellowship(){
+    return "friends";
+  }
+  var sword = "sting";
+  var dwarf = function(){ 
+    return "axe";
+  };
+  var fall = "Fly you fools!";
+  fellowship = function(){
+    return "broken";
+  };
+  ring();
+  return sword;
+  fellowship = function(){
+    return "mines"
+  };
+  sword = function(){
+    return "glamdring";
+  };
+  var ring = function(){
+      return "precious";
+  };
+  
+}
+
 print$(theBridgeOfHoistingDoom());
-
-
-
