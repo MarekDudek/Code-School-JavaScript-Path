d866528fb7f44e3167f96b4cc1b57951573a3c35
Marek Dudek <marek.dudek@gmail.com>
2015-02-01 17:50:33 +0100

    Hoisting Hills challenges cleared

--- javascript-road-trip/part-3/hoisting-hills.js
+++ javascript-road-trip/part-3/hoisting-hills.js
@@ -98,29 +98,61 @@ function theBridgeOfHoistingDoom () {
 
 
 function theBridgeOfHoistingDoom( ){
+	
+	var sword = undefined;
+	var dwarf = undefined;
+	var fall = undefined; 
+	var ring = undefined;
+
+	function fellowship(){
+		return "friends";
+	}
+
+	sword = "sting";
+
+	dwarf = function(){ 
+		return "axe";
+	};
+
+	fall = "Fly you fools!";
+
+	fellowship = function(){
+		return "broken";
+	};
+
+	ring = function(){
+		return "precious";
+	};
+
+	ring();
+
+	return sword;
+
+	fellowship = function(){
+		return "mines"
+	};
+}
+
+//print$(theBridgeOfHoistingDoom());
+
+
+function theBridgeOfHoistingDoom( ){
+  var sword = undefined;
+  var dwarf = undefined;
+  var fall = undefined;
+  var ring = undefined;
   function fellowship(){
     return "friends";
   }
-  var sword = "sting";
-  var dwarf = function(){ 
+  sword = "sting";
+  dwarf = function (){ 
     return "axe";
-  };
-  var fall = "Fly you fools!";
-  fellowship = function(){
+  }
+  fall = "Fly you fools!";
+  fellowship = function (){
     return "broken";
-  };
+  }
   ring();
   return sword;
-  fellowship = function(){
-    return "mines"
-  };
-  sword = function(){
-    return "glamdring";
-  };
-  var ring = function(){
-      return "precious";
-  };
-  
 }
-
 print$(theBridgeOfHoistingDoom());
