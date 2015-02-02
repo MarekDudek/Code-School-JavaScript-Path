279f35da82de87260ac02ddc605ca0133cb39fcc
Marek Dudek <marek.dudek@gmail.com>
2015-02-02 22:49:50 +0100

    Ternary operator

--- src/test/javascript/best-practices/the-sword-of-syntax.js
+++ src/test/javascript/best-practices/the-sword-of-syntax.js
@@ -1,8 +1,14 @@
+describe('The Sword of Syntax ...', function () {
 
-describe('The Sword of Syntax ...',function() {
+    describe('Ternary conditional', function () {
 
-    it('... was raised', function() {
-        var variable = 3;
-        expect(variable).toBe(3);
+        it('is far shorter', function () {
+
+            var isArthur = false;
+
+            var weapon = isArthur ? 'Excalibur' : 'Longsword';
+
+            expect(weapon).toBe('Longsword');
+        });
     });
 });
